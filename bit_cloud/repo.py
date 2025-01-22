2b9db2a9296a28638b451fa38ab58ce7f952227d
manavboss1 <167421071+manavboss1@users.noreply.github.com>
2025-01-22 19:05:22 +0530

    Create repo.py

---
+++ bit_cloud/repo.py
@@ -0,0 +1,37 @@
+import typer
+from pathlib import Path
+from enum import Enum
+
+app = typer.Typer()
+
+class ObjectType(str, Enum):
+    BLOB = "blob"
+    TREE = "tree"
+    COMMIT = "commit"
+    TAG = "tag"
+
+@app.command(help="Read contents of the bit object", rich_help_panel="inspect")
+def show(type: ObjectType = typer.Option(ObjectType.BLOB, "-t", "--type", show_choices=True), file: Path = typer.Argument(..., exists=True)):
+    typer.echo(f"cat_file: {type}")
+
+@app.command(help="hash the file and (optionally) write it to the object store", rich_help_panel="inspect")
+def hash(file: Path = typer.Argument(..., exists=True), write: bool = typer.Option(False, "-w", "--write"), type: ObjectType = typer.Option(ObjectType.BLOB, "-t", "--type", show_choices=True)):
+    typer.echo(f"hash_object: {file} {write} {type}")
+
+@app.command(help="initialize a new bit repository", rich_help_panel="creation")
+def init():
+    typer.echo("init")
+    dire = Path(".bit")
+    curr = Path(".")
+    dire.mkdir(exist_ok=True)
+    (dire / "objects").mkdir()
+    (dire / "config.toml").write_text(
+        f"""
+[repo]
+name = {curr.name}
+ctr = 0
+"""
+    )
+
+if __name__ == "__main__":
+    app()
